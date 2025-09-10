b0c34248c2398b693ababd3fd183c1216d47bf61
RYXION <132704345+Alanwolker8989@users.noreply.github.com>
2025-09-10 11:09:55 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from handlers import router
-from database import init_db   # <---- добавьте этот импорт
+from database import init_db   
 
 bot = Bot(token='8351177209:AAERJ5F15zT2dX3ZK1Fc4LMDrvEUi3E-kG4')
 dp = Dispatcher()
